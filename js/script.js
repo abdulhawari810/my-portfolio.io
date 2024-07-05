a210a79cd8772ebf7d65cb620146bd9f9c712d70
abdulhawari810 <abdulhawari810@gmail.com>
2024-07-06 00:30:24 +0700

    updated

--- js/script.js
+++ js/script.js
@@ -3,7 +3,11 @@ const nav = document.querySelectorAll(".navbar-items"),
   toggle = document.querySelector(".toggle"),
   navbar = document.querySelector(".nav");
 
-for (let i = 0; i < nav.length && i < divider.length; i++) {
+  for (let i = 0; i < nav.length && i < divider.length; i++) {
+    if(window.location.href === "http://127.0.0.1:5500/portfolio/index.html"){
+      nav[0].classList.add("active");
+      divider[0].classList.add("active");
+  }
   if (nav[i].href === window.location.href) {
     nav[i].classList.add("active");
     divider[i].classList.add("active");
